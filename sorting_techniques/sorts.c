d13f8ff5585bb508e60bc4365cc3905d3fe4aab0
Shuxing Li <hingsmy@gmail.com>
2020-06-23 10:45:52 +0800

    selection sort

--- sorting_techniques/sorts.c
+++ sorting_techniques/sorts.c
@@ -44,11 +44,26 @@ void Insertion(int A[], int n)
   }
 }
 
+void SelectionSort(int A[], int n)
+{
+  int i, j, k;
+
+  for (i = 0; i < n - 1; i++)
+  {
+    for (j = k = i; j < n; j++)
+    {
+      if (A[j] < A[k])
+        k = j;
+    }
+    swap(&A[i], &A[k]);
+  }
+}
+
 int main()
 {
-  int A[] = {11, 13, 7, 2, 6, 9, 4, 5, 10, 3}, n = 10, i;
+  int A[] = {11, 13, 7, 12, 16, 9, 4, 5, 10, 3}, n = 10, i;
 
-  Insertion(A, n);
+  SelectionSort(A, n);
 
   for (i = 0; i < 10; i++)
     printf("%d ", A[i]);
